58396e34fc73086c8d2a7f4d357b30b0de13d60e
nobodyiam <nobodyiam@gmail.com>
2015-06-14 16:42:08 +0800

    add netty nio server

--- netty/src/main/java/NettyNioServer.java
+++ netty/src/main/java/NettyNioServer.java
@@ -3,9 +3,9 @@ import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.*;
-import io.netty.channel.oio.OioEventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
-import io.netty.channel.socket.oio.OioServerSocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,19 +14,20 @@ import java.io.IOException;
 /**
  * Created by Jason on 6/14/15.
  */
-public class NettyOioServer {
-    private static final Logger logger = LoggerFactory.getLogger(NettyOioServer.class);
+public class NettyNioServer {
+    private static final Logger logger = LoggerFactory.getLogger(NettyNioServer.class);
 
     public void serve(int port) throws InterruptedException {
-        final ByteBuf buf = Unpooled.unreleasableBuffer(Unpooled.copiedBuffer("Hi!\r\n", Charsets.UTF_8));
-        EventLoopGroup group = new OioEventLoopGroup(); // Use OioEventLoopGroup to allow blocking mode (Old-IO)
+        final ByteBuf buf = Unpooled.copiedBuffer("Hi!\r\n", Charsets.UTF_8);
+        EventLoopGroup group = new NioEventLoopGroup(); // Use NioEventLoopGroup for nonblocking mode
 
         try {
-            ServerBootstrap b = new ServerBootstrap(); // Create ServerBootstrap to allow bootstrap to server instance
+            ServerBootstrap b = new ServerBootstrap(); // Create ServerBootstrap to allow bootstrap to server
+
             b.group(group)
-                    .channel(OioServerSocketChannel.class)
+                    .channel(NioServerSocketChannel.class)
                     .localAddress(port)
-                    .childHandler(new ChannelInitializer<SocketChannel>() { // Specify ChannelInitializer that will be called for each accepted connection
+                    .childHandler(new ChannelInitializer<SocketChannel>() { // Specify ChannelInitializer called for each accepted connection
                         @Override
                         protected void initChannel(SocketChannel ch) throws Exception {
                             ch.pipeline().addLast(new ChannelInboundHandlerAdapter() { // Add ChannelHandler to intercept events and allow to react on them
@@ -39,7 +40,7 @@ public class NettyOioServer {
                         }
                     });
             ChannelFuture f = b.bind().sync(); // Bind server to accept connections
-            logger.info(NettyOioServer.class.getName() + " started and listen on " + f.channel().localAddress());
+            logger.info(NettyNioServer.class.getName() + " started and listen on " + f.channel().localAddress());
             f.channel().closeFuture().sync();
         } finally {
             group.shutdownGracefully().sync(); // Release all resources
@@ -48,10 +49,10 @@ public class NettyOioServer {
 
     public static void main(String[] args) throws IOException, InterruptedException {
         if (args.length != 1) {
-            logger.error("Usage: " + NettyOioServer.class.getSimpleName() + " <port>");
+            logger.error("Usage: " + NettyNioServer.class.getSimpleName() + " <port>");
             return;
         }
         int port = Integer.parseInt(args[0]);
-        new NettyOioServer().serve(port);
+        new NettyNioServer().serve(port);
     }
 }
