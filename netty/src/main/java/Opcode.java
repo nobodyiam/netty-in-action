cdfbeba60fe283bef6e283631b168518a1d4166f
nobodyiam <nobodyiam@gmail.com>
2015-07-04 10:11:02 +0800

    add memcached codec

---
+++ netty/src/main/java/Opcode.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Jason on 7/4/15.
+ */
+public class Opcode {
+    public static final byte GET = 0x00;
+    public static final byte SET = 0x01;
+    public static final byte DELETE = 0x04;
+}
